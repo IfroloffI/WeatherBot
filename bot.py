28a1a6e86797fab00eb93eb28ab37490af2934ec
Zephrys_TheGreatest <froloff1830@gmail.com>
2024-09-12 04:53:46 +0300

    UPDATE | add new message handlers

--- bot.py
+++ bot.py
@@ -2,16 +2,21 @@ from telebot import *
 from utils.tokens import *
 import requests
 
+is_started = False
+
+
 def get_weather(cords):
     lat, lon = cords
     try:
         headers = {
             'X-Yandex-Weather-Key': get_weather_token()
         }
-        request = requests.get("https://api.weather.yandex.ru/v2/forecast?lat=" + str(lat) + "&lon=" + str(lon), headers=headers).json()
+        request = requests.get("https://api.weather.yandex.ru/v2/forecast?lat=" + str(lat) + "&lon=" + str(lon),
+                               headers=headers).json()
         return request["fact"]
     except:
         return "Error"
+
 
 def get_coords_by_name(name):
     map_token = get_map_token()
@@ -19,22 +24,25 @@ def get_coords_by_name(name):
     response_text = "https://geocode-maps.yandex.ru/1.x/?apikey=" + map_token + "&geocode=" + name + "&format=json"
     request = requests.get(response_text).json()
     try:
-        coords = request["response"]["GeoObjectCollection"]["metaDataProperty"]["GeocoderResponseMetaData"]["boundedBy"]["Envelope"]
-        lower_x, lower_y = map(float, coords["lowerCorner"].split())
-        upper_x, upper_y = map(float, coords["upperCorner"].split())
+        cords = request["response"]["GeoObjectCollection"]["metaDataProperty"]["GeocoderResponseMetaData"]["boundedBy"][
+            "Envelope"]
+        lower_x, lower_y = map(float, cords["lowerCorner"].split())
+        upper_x, upper_y = map(float, cords["upperCorner"].split())
         x, y = (lower_x + upper_x) / 2, (lower_y + upper_y) / 2,
         return (x, y)
     except:
         return "Error"
 
+
 def run():
     bot = telebot.TeleBot(get_bot_token())
 
-    @bot.message_handler(commands=["start"])
+    @bot.message_handler(commands=["start", "go"])
     def start(message):
+        global is_started
+        is_started = True
         keyboard = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
-        print(get_coords_by_name("Москва"))
-        print(get_weather((37.620393, 55.751475)))
+
         button_geo = types.KeyboardButton(text="Отправить местоположение", request_location=True)
         button_input = types.KeyboardButton(text="Ввести город")
 
@@ -43,8 +51,25 @@ def run():
 
         bot.send_message(message.chat.id, "Поделитесь местоположением", reply_markup=keyboard)
 
+    @bot.message_handler(content_types=['text'])
+    def weather_by_adress(message):
+        global is_started
+        if is_started:
+            adress = message.text
+            is_started = False
+            try:
+                lat, lon = get_coords_by_name(adress)
+                data = get_weather((lat, lon))
+                bot.send_message(message.chat.id, data)
+            except:
+                bot.send_message(message.chat.id, "Ошибка ввода локации!")
+
     @bot.message_handler(content_types=['location'])
-    def location(message):
-        print(get_weather().json())
+    def weather_by_location(message):
+        global is_started
+        is_started = False
+        lat, lon = message.location.latitude, message.location.longitude
+        data = get_weather((lat, lon))
+        bot.send_message(message.chat.id, data)
 
     bot.polling(none_stop=True, interval=0)
