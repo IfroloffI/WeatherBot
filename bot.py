ac0f56ef4c1fc322aa759908f4330ac82ee31cbb
Zephrys_TheGreatest <froloff1830@gmail.com>
2024-09-14 10:18:34 +0300

    UPDATE | update location search

--- bot.py
+++ bot.py
@@ -2,8 +2,6 @@ from telebot import *
 from utils.tokens import *
 import requests
 
-is_started = False
-
 
 def get_weather(cords):
     lat, lon = cords
@@ -18,12 +16,18 @@ def get_weather(cords):
         return "Error"
 
 
-def get_coords_by_name(name):
+def parse_weather(data):
+    # TODO: сделать красивый вывод
+    return str(data)
+
+
+def get_cords_by_name(name):
     map_token = get_map_token()
     name = '+'.join(name.split())
     response_text = "https://geocode-maps.yandex.ru/1.x/?apikey=" + map_token + "&geocode=" + name + "&format=json"
     request = requests.get(response_text).json()
     try:
+        # TODO: проблемы с поиском города
         cords = request["response"]["GeoObjectCollection"]["metaDataProperty"]["GeocoderResponseMetaData"]["boundedBy"][
             "Envelope"]
         lower_x, lower_y = map(float, cords["lowerCorner"].split())
@@ -39,8 +43,6 @@ def run():
 
     @bot.message_handler(commands=["start", "go"])
     def start(message):
-        global is_started
-        is_started = True
         keyboard = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
 
         button_geo = types.KeyboardButton(text="Отправить местоположение", request_location=True)
@@ -53,23 +55,22 @@ def run():
 
     @bot.message_handler(content_types=['text'])
     def weather_by_adress(message):
-        global is_started
-        if is_started:
-            adress = message.text
-            is_started = False
-            try:
-                lat, lon = get_coords_by_name(adress)
-                data = get_weather((lat, lon))
-                bot.send_message(message.chat.id, data)
-            except:
-                bot.send_message(message.chat.id, "Ошибка ввода локации!")
+        adress = message.text
+        if '/' in adress or adress == "Ввести город":
+            return
+        try:
+            lat, lon = get_cords_by_name(adress)
+            data = get_weather((lat, lon))
+            text = parse_weather(data)
+            bot.send_message(message.chat.id, text)
+        except:
+            bot.send_message(message.chat.id, "Ошибка ввода локации!")
 
     @bot.message_handler(content_types=['location'])
     def weather_by_location(message):
-        global is_started
-        is_started = False
         lat, lon = message.location.latitude, message.location.longitude
         data = get_weather((lat, lon))
-        bot.send_message(message.chat.id, data)
+        text = parse_weather(data)
+        bot.send_message(message.chat.id, text)
 
     bot.polling(none_stop=True, interval=0)
